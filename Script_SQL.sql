4108e8c80832ccaffbd68a7b35acec171dd296e5
RupoloWillian <willianrupolo2@gmail.com>
2023-07-04 22:24:25 -0300

    Update queries

--- Script_SQL.sql
+++ Script_SQL.sql
@@ -16,6 +16,32 @@ CREATE TABLE bike_share.tb_bike(
     member_casual VARCHAR(10) NOT NULL
 );
 
+/*Habilitando o SAFEUPDATE*/
+SET SQL_SAFE_UPDATES = 0;
+~~~
+
+/* Tratando os valores em branco */
+~~~SQL
+UPDATE bikeshare.tb_bike
+SET start_station_name = 'S/L'
+WHERE start_station_name = '';
+
+UPDATE bikeshare.tb_bike
+SET start_station_id = 'S/N'
+WHERE start_station_id = '';
+
+UPDATE bikeshare.tb_bike
+SET end_station_name = 'S/L'
+WHERE end_station_name = '';
+
+UPDATE bikeshare.tb_bike
+SET end_station_id = 'S/N'
+WHERE end_station_id = '';
+~~~
+
+/* Dasabilitando o SAFE UPDATE MODEL */
+~~~SQL
+SET SQL_SAFE_UPDATES = 1;
 
 /*1) Qual tipo de bicicleta foi a mais utilizada e qual a menos utilizada?​*/
 
@@ -45,18 +71,28 @@ ORDER BY Quantidade_Emprestimos DESC;
 SELECT 
 	DAY(started_at) AS Dia, 
 	COUNT(ride_id) AS Quantidade_Emprestimos, 
-    ROUND(AVG(TIMESTAMPDIFF(MINUTE, started_at, ended_at)),2) AS Media_Duracao_Min
+    ROUND(
+		AVG(TIMESTAMPDIFF(MINUTE, started_at, ended_at)),2) AS Media_Duracao_Min
 FROM bike_share.tb_bike
 GROUP BY Dia
 ORDER BY Quantidade_Emprestimos DESC;
 
 
-/*4) Quais é o horário de pico no empréstimo de bicicletas?*/
+/*4) Quais é o horário de pico no empréstimo de bicicletas para cada tipo de membro?*/
 
 SELECT 
 	HOUR(started_at) AS Hora, 
 	COUNT(ride_id) AS Quantidade_Emprestimos
 FROM bike_share.tb_bike
+WHERE member_casual = "member"
+GROUP BY Hora 
+ORDER BY Quantidade_Emprestimos DESC;
+
+SELECT 
+	HOUR(started_at) AS Hora, 
+	COUNT(ride_id) AS Quantidade_Emprestimos
+FROM bike_share.tb_bike
+WHERE member_casual = "casual"
 GROUP BY Hora 
 ORDER BY Quantidade_Emprestimos DESC;
 
@@ -66,7 +102,37 @@ ORDER BY Quantidade_Emprestimos DESC;
 SELECT 
 	member_casual AS Tipo_Membro, 
     COUNT(ride_id) AS Quantidade_Emprestimos,
-    ROUND(COUNT(ride_id)/(SELECT COUNT(ride_id) FROM bike_share.tb_bike),2)*100 AS Quantidade_Emprestimos_Porc,
-	ROUND(AVG(TIMESTAMPDIFF(MINUTE, started_at, ended_at)),2) AS Media_Duracao_Min
+    ROUND(
+		(COUNT(ride_id)/(SELECT COUNT(ride_id) FROM bike_share.tb_bike))*100, 2) 
+		AS Quantidade_Emprestimos_Porc,
+	ROUND(
+		AVG(TIMESTAMPDIFF(MINUTE, started_at, ended_at)),2) AS Media_Duracao_Min
 FROM bike_share.tb_bike
 GROUP BY member_casual;
+
+/*6)Qual foi a quantidade de aluguéis realizadas em cada mês? Qual foi a variação em relação ao mês anterior?*/
+SELECT
+	Mes,
+    Quantidade_Emprestimos,
+    Quantidade_Emprestimos_LM,
+    ROUND((Dif/Quantidade_Emprestimos_LM)*100, 2) AS Diferenca
+FROM(
+	/*Cria uma tabela virtual com o mês, a Quantidade de Empréstimos/mês, 
+	Quantidade de Empréstimos Mês Anterior e a Variação da Quantidade de Empréstimos em relação ao Mês Anterior*/
+	SELECT 
+		Mes,
+		Quantidade_Emprestimos,
+		LAG(Quantidade_Emprestimos) OVER(ORDER BY Mes ASC) AS Quantidade_Emprestimos_LM,
+		Quantidade_Emprestimos - LAG(Quantidade_Emprestimos) OVER(ORDER BY Mes ASC) AS Diferenca
+	FROM (
+		/*Cria uma tabela virtual com o mês e a Quantidade de Empréstimos/mês*/
+		SELECT 
+			MONTH(started_at) AS Mes,
+			COUNT(ride_id) AS Quantidade_Emprestimos
+		FROM bike_share.tb_bike
+		GROUP BY Mes
+		) AS Tabela
+	) AS Resultado
+WHERE Resultado.Quantidade_Emprestimos_LM IS NOT NULL
+GROUP BY Mes
+ORDER BY Mes ASC;
